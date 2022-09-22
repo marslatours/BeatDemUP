db4d716dcc50c7b877e665792f1c28f6248068fd
Robynson Siguenay <marslatours@gmail.com>
2022-09-22 12:28:08 +0200

    healthhhhhhhhbrahhhhhhhhh

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -6,10 +6,12 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
+    private Animator anim;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(float _damage) 
@@ -17,12 +19,14 @@ public class Health : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
 
         if (currentHealth > 0) 
-        { 
+        {
             //player hurt
+            anim.SetTrigger("hurt");
         }
         else 
-        { 
+        {
             //player dead
+            anim.SetTrigger("death");
         }
 
 
